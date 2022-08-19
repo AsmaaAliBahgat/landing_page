0a1b3658aca649db1c41266a37bb6c2140b0acae
Asmaa Bahgat <55767745+AsmaaAliBahgat@users.noreply.github.com>
2022-08-19 21:08:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img'),
+container = document.querySelector('.container');
+function coffes (coffee){
+    img.src = coffee;
+}
+function background (color){
+    container.style.background = color;
+}
